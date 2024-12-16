8a10ad62453b564b8995b67978182252c66e785f
JeonghyunKim <kimdictor@gmail.com>
2024-12-16 21:29:06 +0900

    Write position and visual node

--- install/pubsub/lib/python3.8/site-packages/pubsub/nAruco.py
+++ install/pubsub/lib/python3.8/site-packages/pubsub/nAruco.py
@@ -1,21 +1,32 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Float32MultiArray  
+from std_msgs.msg import String, Float32MultiArray, Int32  
 
 
 class nAruco(Node):
     def __init__(self):
         super().__init__('aruco')
-        self.publisher_ = self.create_publisher(Float32MultiArray, 'angle', 10)
+        self.angle_publisher_ = self.create_publisher(Float32MultiArray, 'angle', 10)
+        self.id_publisher_ = self.create_publisher(Int32, 'id', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
         self.vector = [0.0, 0.0, 0.0]
+        self.id = -1
 
     def timer_callback(self):
-        self.vector = read_vector()
+        marker = read_marker()
+        self.vector = marker[0]
+        self.id = marker[1]
+
         msg = Float32MultiArray()
         msg.data = self.vector
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.angle_publisher_.publish(msg)
+        self.get_logger().info('Publishing angle: "%s"' % msg.data)
+
+        msg = Int32()
+        msg.data = self.id
+        self.id_publisher_.publish(msg)
+        self.get_logger().info('Publishing id: %d' % msg.data)
+
 
 
 def main(args=None):
@@ -25,9 +36,9 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
-def read_vector():
+def read_marker():
     # Fill here
-    return [0.0, 0.0, 0.0]
+    return ([0.0, 0.0, 0.0], 0)
 
 
 if __name__ == '__main__':
